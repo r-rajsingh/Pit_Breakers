01ac0ab2c9d5281bef1b33897956cd34e9f10756
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2019-04-09 23:46:06 +0530

    Added data posting in mongodb

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 
 const port = process.env.PORT || 1337;
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/Pits_DB";
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
@@ -14,9 +17,53 @@ app.get('/', function(req, res){
 });
 
 app.post('/data', function(request, response){
-  console.log(request.body);
+  console.log('\n Data posted succesfully');
+  console.log(request.body.location);
   response.contentType('json');
   response.send(JSON.stringify({status: "success", data: request.body}, null, 2));
+
+  MongoClient.connect(url, { useNewUrlParser: true },function(err, client){
+
+    if(err) throw err;
+    const db = client.db('Pits_DB')
+
+    db.collection('Location').insertMany(request.body.location, (err,result)=>{
+      if(err){
+        return console.log('Unable to insert location to Pits_DB',err);
+      }
+      return console.log('Location inserted successfully');
+    });
+
+    db.collection('Linear_acc').insertMany(request.body.linear_acc, (err,result)=>{
+      if(err){
+        return console.log('Unable to insert linear_acc to Pits_DB',err);
+      }
+      return console.log('linear_acc inserted successfully');
+    });
+
+    db.collection('Gravity_acc').insertMany(request.body.gravity_acc, (err,result)=>{
+      if(err){
+        return console.log('Unable to insert gravity_acc to Pits_DB',err);
+      }
+      return console.log('gravity_acc inserted successfully');
+    });
+
+    db.collection('Euler_angle').insertMany(request.body.euler_angle, (err,result)=>{
+      if(err){
+        return console.log('Unable to insert euler_angle to Pits_DB',err);
+      }
+      return console.log('euler_angle inserted successfully');
+    });
+
+    db.collection('Angular_acc').insertMany(request.body.angular_acc, (err,result)=>{
+      if(err){
+        return console.log('Unable to insert angular_acc to Pits_DB',err);
+      }
+      return console.log('angular_acc inserted successfully');
+    });
+
+    client.close();
+   });
 });
 
 app.listen(port, ()=>{
